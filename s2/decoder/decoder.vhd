6ad0d11d0add00e695d125de1e7f07327df25cb2
Parham Alvani <parham.alvani@gmail.com>
2016-02-15 14:20:39 +0330

    Behavioural architecture was added.

--- s2/decoder/decoder.vhd
+++ s2/decoder/decoder.vhd
@@ -8,14 +8,24 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity decoder is
-	port (i0, i1 : in std_logic;
-		o0, o1, o2, o3 : out std_logic);
+	port (i : in std_logic_vector (1 downto 0);
+		o : out std_logic_vector (3 downto 0));
 end entity;
 
 architecture gate_arch_decoder of decoder is
 begin
-	o0 <= (not i0) and (not i1);
-	o1 <= i0 and (not i1);
-	o2 <= (not i0) and i1;
-	o3 <= i0 and i1;
+	o(0) <= (not i(0)) and (not i(1));
+	o(1) <= i(0) and (not i(1));
+	o(2) <= (not i(0)) and i(1);
+	o(3) <= i(0) and i(1);
 end architecture;
+
+architecture beh_arch_decoder of decoder is
+begin
+	with i select
+		o <= "0001" when "00",
+		     "0010" when "01",
+		     "0100" when "10",
+		     "1000" when "11",
+		     "XXXX" when others;
+end architecture beh_arch_decoder;
--- s2/decoder/decoder_t.vhd
+++ s2/decoder/decoder_t.vhd
@@ -12,14 +12,14 @@ end entity decoder_t;
 
 architecture arch_decoder_t of decoder_t is
 	component decoder is
-		port (i0, i1 : in std_logic;
-			o0, o1, o2, o3 : out std_logic);
+		port (i : in std_logic_vector(1 downto 0);
+			o : out std_logic_vector(3 downto 0));
 	end component decoder;
-	signal i0, i1 : std_logic;
-	signal o0, o1, o2, o3 : std_logic;
-	for all:decoder use entity work.decoder(gate_arch_decoder);
+	signal i : std_logic_vector(1 downto 0);
+	signal o : std_logic_vector(3 downto 0);
+	for all:decoder use entity work.decoder(beh_arch_decoder);
 begin
-	d : decoder port map (i0, i1, o0, o1, o2, o3);
-	i0 <= '0', '1' after 100 ns, '0' after 200 ns, '1' after 300 ns;
-	i1 <= '0', '0' after 100 ns, '1' after 200 ns, '1' after 300 ns;
+	d : decoder port map (i, o);
+	i(0) <= '0', '1' after 100 ns, '0' after 200 ns, '1' after 300 ns;
+	i(1) <= '0', '0' after 100 ns, '1' after 200 ns, '1' after 300 ns;
 end architecture arch_decoder_t;
