65feae7354c251ac0c032ed69df88ca0018a1a93
Parham Alvani <parham.alvani@gmail.com>
2016-02-15 21:00:25 +0330

    Using generic variable in 4-bit-adder

--- s2/4-bit-adder/4-bit-adder.vhd
+++ s2/4-bit-adder/4-bit-adder.vhd
@@ -8,9 +8,10 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity four_bit_adder is
-	port (a, b : in std_logic_vector(3 downto 0);
+	generic(N : natural:=4);
+	port (a, b : in std_logic_vector(N - 1 downto 0);
 		c_in : in std_logic;
-		sum : out std_logic_vector(3 downto 0);
+		sum : out std_logic_vector(N - 1 downto 0);
 		c_out : out std_logic);
 end entity;
 
@@ -19,12 +20,12 @@ architecture arch_four_bit_adder of four_bit_adder is
 		port (a, b, c_in : in std_logic;
 			sum, c_out : out std_logic);
 	end component fulladdr;
-	signal c : std_logic_vector(4 downto 0);
+	signal c : std_logic_vector(N downto 0);
 	for all:fulladdr use entity work.fulladdr(arch_fulladdr);
 begin
 	c(0) <= c_in;
-	c_out <= c(4);
-	F : for I in 0 to 3 generate
+	c_out <= c(N);
+	F : for I in 0 to N - 1 generate
 		fas : fulladdr port map (a(I), b(I), c(I), sum(I), c(I + 1));
 	end generate F;
 end architecture arch_four_bit_adder;
