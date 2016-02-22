d455a06a31133e544adbf87b14d2c59087db03e9
Parham Alvani <parham.alvani@gmail.com>
2016-02-22 16:05:33 +0330

    counter asynchronous reset was added.

--- s3/counter/counter.vhd
+++ s3/counter/counter.vhd
@@ -11,14 +11,18 @@ use IEEE.std_logic_unsigned.all;
 entity counter is
 	generic (N : integer := 4);
 	port (number : out std_logic_vector (N - 1 downto 0) := (others => '0');
-		clk : in std_logic);
+		clk, r : in std_logic);
 end entity counter;
 
 architecture beh_arch_counter of counter is
 begin
-	process (clk)
+	process (clk, r)
 		variable old_number : std_logic_vector (N - 1 downto 0) := (others => '0');
 	begin
+		if r = '1' then
+			number <= (others => '0');
+			old_number := (others => '0');
+		end if;
 		if clk = '1' and clk'event then
 			old_number := old_number + (0 => '1');
 			number <= old_number;
