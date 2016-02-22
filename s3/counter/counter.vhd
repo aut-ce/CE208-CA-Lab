47be9a822a49519d5a6a7dd3fc1855d410ae4205
Parham Alvani <parham.alvani@gmail.com>
2016-02-22 15:46:05 +0330

    Using variable in counter.

--- s3/counter/counter.vhd
+++ s3/counter/counter.vhd
@@ -10,16 +10,18 @@ use IEEE.std_logic_unsigned.all;
 
 entity counter is
 	generic (N : integer := 4);
-	port (number : buffer std_logic_vector (N - 1 downto 0) := (others => '0');
+	port (number : out std_logic_vector (N - 1 downto 0) := (others => '0');
 		clk : in std_logic);
 end entity counter;
 
 architecture beh_arch_counter of counter is
 begin
 	process (clk)
+		variable old_number : std_logic_vector (N - 1 downto 0) := (others => '0');
 	begin
 		if clk = '1' and clk'event then
-			number <= number + (0 => '1');
+			old_number := old_number + (0 => '1');
+			number <= old_number;
 		end if;
 	end process;
 end architecture beh_arch_counter;
