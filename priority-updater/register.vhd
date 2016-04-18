32859f0a9def4e735f731b33711b1519efcb4006
Parham Alvani <parham.alvani@gmail.com>
2016-04-18 14:39:08 +0430

    Load signal was added into register.

--- priority-updater/register.vhd
+++ priority-updater/register.vhd
@@ -10,26 +10,16 @@ use IEEE.std_logic_1164.all;
 entity n_register is
 	generic (N : integer := 4);
 	port (d : in std_logic_vector(N - 1 downto 0);
-		clk, s_sync, r_sync : in std_logic;
-		s_async, r_async : in std_logic;
+		clk, load : in std_logic;
 		q : out std_logic_vector(N - 1 downto 0));
 end entity n_register;
 
 architecture behavior of n_register is
 begin
-	process (clk, s_async, r_async)
+	process (clk)
 	begin
-		if s_async = '1' then
-			q <= (others => '1');
-		elsif r_async = '1' then
-			q <= (others => '0');
-		end if;
 		if clk = '1' and clk'event then
-			if s_sync = '1' then
-				q <= (others => '1');
-			elsif r_sync = '1' then
-				q <= (others => '0');
-			else
+			if load = '1' then
 				q <= d;
 			end if;
 		end if;
