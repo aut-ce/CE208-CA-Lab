6fe0bee7e0200df2367f4664a22ac64b2942eb4b
Parham Alvani <parham.alvani@gmail.com>
2016-04-25 15:02:59 +0430

    Counter module enable signal was added.

--- priority-updater/counter.vhd
+++ priority-updater/counter.vhd
@@ -11,7 +11,7 @@ use IEEE.std_logic_unsigned.all;
 entity counter is
 	generic (N : integer := 4);
 	port (number : out std_logic_vector (N - 1 downto 0) := (others => '0');
-		clk, r : in std_logic);
+		clk, r, en : in std_logic);
 end entity counter;
 
 architecture behavioral of counter is
@@ -23,9 +23,9 @@ begin
 			number <= (others => '0');
 			old_number := (others => '0');
 		end if;
-		if clk = '1' and clk'event then
-			old_number := old_number + (0 => '1');
+		if clk = '1' and clk'event en = '1' then
 			number <= old_number;
+			old_number := old_number + (0 => '1');
 		end if;
 	end process;
 end architecture behavioral;
