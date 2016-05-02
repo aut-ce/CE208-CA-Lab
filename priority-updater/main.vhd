e08369269a8f7974118e48f091a0186a8b6001d4
Parham Alvani <parham.alvani@gmail.com>
2016-05-02 13:54:42 +0430

    Main module was improved.

--- priority-updater/main.vhd
+++ priority-updater/main.vhd
@@ -8,7 +8,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity main is
-	port (clk, free : in std_logic;
+	port (clk, free, reset : in std_logic;
 		address : in std_logic_vector(3 downto 0);
 		done : out std_logic;
 		memory : out std_logic_vector(3 downto 0));
@@ -21,6 +21,7 @@ architecture rtl of main is
 		      	sel_1 : in std_logic;
 			sel_2 : in std_logic_vector(1 downto 0);
 			counter_reset : in std_logic;
+			counter_enable : in std_logic;
 			rwbar : in std_logic;
 			load : in std_logic;
 			counter_done : out std_logic;
@@ -29,9 +30,11 @@ architecture rtl of main is
 	component controller
 		port (g, e, l : in std_logic;
 			clk : in std_logic;
+			reset : in std_logic;
 			sel_1 : out std_logic;
 			sel_2 : out std_logic_vector(1 downto 0);
 			counter_reset : out std_logic;
+			counter_enable : out std_logic;
 			load : out std_logic;
 			counter_done : in std_logic;
 			free: in std_logic;
@@ -41,9 +44,9 @@ architecture rtl of main is
 	for all:datapath use entity work.datapath;
 	for all:controller use entity work.controller;
 	
-	signal g, e, l, sel_1, counter_reset, rwbar, counter_done, load : std_logic;
+	signal g, e, l, sel_1, counter_reset, counter_enable, rwbar, counter_done, load : std_logic;
 	signal sel_2 : std_logic_vector(1 downto 0);
 begin
-	dp : datapath port map(g, e, l, clk, sel_1, sel_2, counter_reset, rwbar, load, counter_done, address);
-	cc : controller port map(g, e, l, clk, sel_1, sel_2, counter_reset, load, counter_done, free, done, rwbar);
+	dp : datapath port map(g, e, l, clk, sel_1, sel_2, counter_reset, counter_enable, rwbar, load, counter_done, address);
+	cc : controller port map(g, e, l, clk, reset, sel_1, sel_2, counter_reset, counter_enable, load, counter_done, free, done, rwbar);
 end architecture;
