172b00518e23b07857cab2b66c8c3e81af10eb80
Parham Alvani <parham.alvani@gmail.com>
2016-05-23 13:58:39 +0430

    Improve memory module.

--- AUT-MIPS/memory.vhd
+++ AUT-MIPS/memory.vhd
@@ -12,7 +12,7 @@ entity memory is
 	port (address : in std_logic_vector;
 		data_in : in std_logic_vector;
 		data_out : out std_logic_vector;
-		clk, rwbar : in std_logic);
+		clk, read, write : in std_logic);
 end entity memory;
 
 architecture behavioral of memory is
@@ -23,14 +23,16 @@ begin
 		variable memory : mem (0 to memsize - 1, data_in'range);
 	begin
 		if  clk'event and clk = '1' then
-			if rwbar = '1' then -- Readiing :)
+			if read = '1' and write = '0' then -- Readiing :)
 				for i in data_out'range loop
 					data_out(i) <= memory (to_integer(unsigned(address)), i);
 				end loop;
-			else -- Writing :)
+			elsif read = '0' and write = '1' then -- Writing :)
 				for i in data_in'range loop
 					memory (to_integer(unsigned(address)), i) := data_in (i);
 				end loop;
+			else
+				data_out <= (others => 0);
 			end if;
 		end if;
 	end process;
