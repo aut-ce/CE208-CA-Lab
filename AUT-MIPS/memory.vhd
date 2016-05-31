14b11f060417a262044569e5b535eebc95e28bf3
Parham Alvani <parham.alvani@gmail.com>
2016-05-31 09:39:20 +0430

    Memory module convert to very simple module :(

--- AUT-MIPS/memory.vhd
+++ AUT-MIPS/memory.vhd
@@ -9,26 +9,25 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity memory is
-	generic (N : integer := 16;
-		M : integer := 16);
-	port (address : in std_logic_vector (N - 1 downto 0);
-		data_in : in std_logic_vector (M - 1 downto 0);
-		data_out : out std_logic_vector (M - 1 downto 0);
+	port (address : in std_logic_vector (15 downto 0);
+		data_in : in std_logic_vector (15 downto 0);
+		data_out : out std_logic_vector (15 downto 0);
 		clk, read, write : in std_logic);
 end entity;
 
 architecture behavioral of memory is
-	type mem is array (natural range <>) of std_logic_vector (M - 1 downto 0);
+	type mem is array (natural range <>) of std_logic_vector (15 downto 0);
 begin
 	process (clk)
-		constant memsize : integer := 2 ** N;
+		constant memsize : integer := 2 ** 16;
 		variable memory : mem (0 to memsize - 1) := (
-			0 => "0011000000000010"
+			"0011000000000010",
+			others => "0000000000000000"
 		);
 	begin
 		if  clk'event and clk = '1' then
 			if read = '1' then -- Reading :)
-					data_out <= memory(to_integer(unsigned(address)));
+				data_out <= memory(to_integer(unsigned(address)));
 			elsif write = '1' then -- Writing :)
 				memory(to_integer(unsigned(address))) := data_in;
 			else
