040d7f8f3961d4fb42d071973d4588151921eb84
Parham Alvani <parham.alvani@gmail.com>
2016-05-31 17:23:32 +0430

    Correct controller states ..

--- AUT-MIPS/control.vhd
+++ AUT-MIPS/control.vhd
@@ -28,7 +28,7 @@ entity control is
 end control;
 
 architecture rtl of control is
-	type state is (RST, S0, S1, S2, S3, SR0, SR1, SI0, SI10, SI11, SI20, SI210, SI220, SI221, SI30, SI31, SJ0);
+	type state is (RST, S0, S1, S2, S3, SR0, SI0, SI10, SI11, SI20, SI210, SI220, SI221, SI30, SI31, SJ0);
 	signal current_state, next_state : state := RST;
 begin
 	process (clk)
@@ -85,7 +85,7 @@ begin
 				memread <= '0';
 				memwrite <= '0';
 				memtoreg <= "00";
-				IRe <= '1';
+				IRe <= '0';
 				PCscr <= "00";
 				ALUop <= "0000";
 				ALUsrcB <= "01";
@@ -111,31 +111,14 @@ begin
 				regdest <= "00";
 				regwrite <= '0';
 				
-				if op = "0000" then
+				if op = "1111" then
 					next_state <= SR0;
-				elsif op = "1111" then
+				elsif op = "0000" then
 					next_state <= SJ0;
 				else
 					next_state <= SI0;
 				end if;
 			when SR0 =>
-				PCen <= '0';
-				PCwrite <= '0';
-				IorD <= "00";
-				memread <= '0';
-				memwrite <= '0';
-				memtoreg <= "00";
-				IRe <= '0';
-				PCscr <= "00";
-				ALUop <= op;
-				ALUsrcB <= "00";
-				ALUsrcA <= "01";
-				AluFunc <= "01";
-				regdest <= "00";
-				regwrite <= '0';
-
-				next_state <= SR1;
-			when SR1 =>
 				PCen <= '0';
 				PCwrite <= '0';
 				IorD <= "00";
@@ -186,11 +169,11 @@ begin
 				regwrite <= '0';
 				
 				if op (3 downto 2) = "00" and op(1 downto 0) /= "00" then
-					next_state <=SI10;
+					next_state <= SI10;
 				elsif op (3 downto 2) = "01" and op(1 downto 0) /= "11" then
-					next_state <=SI10;
+					next_state <= SI10;
 				elsif op = "0111" or op = "1000"  then
-					next_state <=SI20 ;
+					next_state <= SI20;
 				else
 					next_state <= SI30;
 				end if;
@@ -298,40 +281,6 @@ begin
 				AluFunc <= "01";
 				regdest <= "00";
 				regwrite <= '1';
-
-				next_state <= S0;
-			when SI30 =>
-				PCen <= '0';
-				PCwrite <= '0';
-				IorD <= "00";
-				memread <= '0';
-				memwrite <= '0';
-				memtoreg <= "00";
-				IRe <= '0';
-				PCscr <= "10";
-				ALUop <= op;
-				ALUsrcB <= "10";
-				ALUsrcA <= "00";
-				AluFunc <= "10";
-				regdest <= "00";
-				regwrite <= '0';
-
-				next_state <= SI31;
-			when SI31 =>
-				PCen <= '0';
-				PCwrite <= '1';
-				IorD <= "00";
-				memread <= '0';
-				memwrite <= '0';
-				memtoreg <= "00";
-				IRe <= '0';
-				PCscr <= "01";
-				ALUop <= op;
-				ALUsrcB <= "00";
-				ALUsrcA <= "01";
-				AluFunc <= "01";
-				regdest <= "00";
-				regwrite <= '0';
 
 				next_state <= S0;
 			when others =>
